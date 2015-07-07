56b273b5368b173453c6b00a37560c82b2bd9ce0
Alberto Tablado <alberto@aluki.org>
2015-07-08 00:16:22 +0200

    License change

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'aws-route53'
 maintainer 'Alberto Tablado'
 maintainer_email 'alberto.tablad@ticketbis.com'
-license 'GPL v3'
+license 'Apache v2.0'
 source_url 'https://github.com/ticketbis/chef-aws-route53'
 description 'Manage Route53 entries'
 long_description IO.read(File.join(
